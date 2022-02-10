1649ab8754961913d41c35b20a5a7c26eb537518
jixianmin <khm9331@naver.com>
2022-02-10 18:23:53 +0900

    0210
    수업끝

---
+++ javascript/JS/change-color.js
@@ -0,0 +1,9 @@
+/*var(변수)var는 요즘 안쓴다? js에서 소대문자 상관없는가?*/
+/*파일이름 대문자?*/
+/*상수 작동하다 html에 아이디 선택*/
+var heading = document.querySelector("#heading");
+heading.style.color = "red";
+heading.onclick = function () {
+  heading.style.color = "blue";
+};
+//콘솔에서 a=3 결과얻기 아웃풋 산술? 함수 = 어떤 동작을 하는 코드들의 묶음//
